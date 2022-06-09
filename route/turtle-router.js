26700af68239b10cc75629ea81d271c45bdbff6a
voytus <voytus@protonmail.com>
2022-06-09 12:45:02 +0100

    WTM: updated

--- route/turtle-router.js
+++ route/turtle-router.js
@@ -11,15 +11,51 @@ router.get(`/getAll`, (request, response, next) => {
         .json(database);
 
 });
-router.post(`/create`);
+router.post(`/create`, (request, response, next) => {
+//data parsed into the request.body object can be assesed anywhere
+// we have access to the request object
+// we must use express.json() or body-poaser() middleware
+
+//if (!request.body) return next({statusCode: 400, message: 'Body cannot be empty'});
+if (Object.keys(request.body).length == 0) return next({statusCode: 400, message: 'Body cannot be empty'});
+
+const newTurtleData = request.body;
+
+newTurtleData.id = idCounter++;
+database.push(newTurtleData);
+
+response.status(201)
+
+});
 router.put(`/update`);
 router.delete(`/delete:id`, (request, response, next) => {
     // acolon followed by name in a patch is a path parameter
     //that can be accesec on the request.params object
-    const id =request.params.id;
+    const id = request.params.id;
     //if req.params was nnot a number id is assigned NaN
+    if (isNaN(id)) return next({statusCode: 400, message: 'ID must be a number'});
+    //find the index of the user in the array by their id
+    //if user doesn't exists
+    // 
 
-    response.status(200).send(id);
+    const index = database.findIndex(function(turtle) {
+        return turtle.Id == id; //find an user Id and match it for existing one 
+        // -1 --calback no id found with the given id
+    if (index == -1) return next({
+        statusCode: 404,
+        message: 'Turtle does not exists with id ${id}'
+    });
+
+    database.splice(index, 1);
+    response.status(200)
+        .send('User deleted succesfully');
+    });
+
+    //     response.status(400).send(`${request.params.id} is not a number`);
+    // }
+    // response.status(200).json({
+    //     id
+    })
 
 let IdCounter = 3;
 const database = [
